4f914f405fec0feb8ce4a5274788301437ad2f57
dwWebApp <123057170+dwWebApp@users.noreply.github.com>
2024-10-21 12:04:51 +0900

    first

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      md: '768px',
+      lg: '1020px',
+    },
+    colors: {
+      'green':'#446750',
+      'beige':'#fffbee',
+      customColor: '#f6f0dc',
+    },
+    backgroundPosition: {
+      'right-bottom': 'right 2rem bottom 2rem',
+      'pc-rightbottom': 'right 3rem bottom 3rem',
+      'center-right': 'center right 9px',
+    },
+    extend: {
+      fontFamily:{
+        pretendard : 'Pretendard',
+      }
+    },
+  },
+  plugins: [],
+}
